6c0db03db0408995dbbb73ef86a6c7d2dba55684
mistersandhu <requesting43@gmail.com>
2025-05-27 15:43:00 +0500

    Hello

---
+++ src/app/layout.js
@@ -0,0 +1,27 @@
+import Preloader from "@/components/ui/preloader";
+import "./global.css"
+import BootstrapForBrowser from "@/components/ui/bootstrapForBrowser";
+import Header from "@/components/sections/header";
+import CallToAction from "@/components/sections/callToAction";
+import Footer from "@/components/sections/footer";
+
+export const metadata = {
+  title: "Muneeb Sandhu - Frontend Developer",
+  keywords: "Muneeb Sandhu, Frontend Developer, React Developer, Portfolio, Web Developer, App Developer",
+  description: "Experienced Frontend Developer specializing in React, Next.js & Flutter. Passionate about creating dynamic, creative and responsive applications. Explore my portfolio to see my work.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning>
+        <BootstrapForBrowser />
+        <Preloader />
+        <Header />
+        {children}
+        <CallToAction />
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ src/components/sections/hero.jsx
@@ -0,0 +1,63 @@
+'use client'
+import React from 'react'
+import Link from 'next/link';
+import { RiFacebookCircleFill, RiTwitterXLine, RiLinkedinFill, RiGithubLine, RiCircleFill, RiDownloadLine, RiInstagramFill, RiInstagramLine } from '@remixicon/react'
+import PartnersMarquee from './partnersMarquee';
+import SlideUp from '@/utlits/animations/slideUp';
+
+const Hero = () => {
+    return (
+        <section id="about" className="about-area">
+            <div className="container">
+                <div className="row">
+                    {/* <!-- START ABOUT IMAGE DESIGN AREA --> */}
+                    <div className="col-lg-4">
+                        <SlideUp>
+                            <div className="about-image-part">
+                                <img src={"/images/about/profile.png"} alt="About Me" />
+                                <h2>Muneeb Sandhu</h2>
+                                <p>Frontend Web & App Developer. Moreover Canva Designer</p>
+                                <div className="about-social text-center">
+                                    <ul>
+                                        <li><Link href=""><RiInstagramLine size={20} /></Link></li>
+                                        <li><Link href=""><RiGithubLine size={20} /></Link></li>
+                                        <li><Link href=""><RiLinkedinFill size={20} /></Link></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </SlideUp>
+                    </div>
+                    {/* <!-- / END ABOUT IMAGE DESIGN AREA -->
+                    <!-- START ABOUT TEXT DESIGN AREA --> */}
+                    <div className="col-lg-8">
+                        <SlideUp>
+                            <div className="about-content-part">
+                                <p>Hello There!</p>
+                                <h2>
+                                    I’m Muneeb Sandhu.
+                                </h2>
+                                <h3>
+                                    Frontend web and app developer specializing in modern, scalable, and animated designs. With extensive experience in React, Next.js, Flutter, and Canva.
+                                </h3>
+                                <div className="adress-field">
+                                    <ul>
+                                        <li className='d-flex align-items-center'><i><RiCircleFill size={14} /></i> Available for Freelancing</li>
+                                    </ul>
+                                </div>
+                                <div className="hero-btns">
+                                    <Link href="/contact" className="theme-btn">Download CV <i><RiDownloadLine size={16} /></i> </Link>
+                                </div>
+                            </div>
+                        </SlideUp>
+                        <SlideUp>
+                            <PartnersMarquee />
+                        </SlideUp>
+                    </div>
+                    {/* <!-- / END ABOUT TEXT DESIGN AREA --> */}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
